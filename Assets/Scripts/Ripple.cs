4fdbe213aaa4acfd77696d7070b9c5b4879c342b
microlm <laura.mo@gmail.com>
2016-03-22 00:43:42 -0700

    First commit

---
+++ Assets/Scripts/Ripple.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ripple : MonoBehaviour {
+
+	public Color lineColor = Color.white;
+	public int degreesPerSegment = 8;
+	public float growthRate = 0.051f;
+	public float fadeRate = 0.01f;
+	public float z = 0f;
+
+	LineRenderer line;
+	CircleCollider2D col;
+
+	private float radialScale;
+	private float opacity;
+	private int vertexCount;
+
+	public Ripple()
+	{
+
+	}
+
+	// Use this for initialization
+	public void Start() {
+		line = GetComponent<LineRenderer> ();
+		line.SetVertexCount (0);
+		vertexCount = 360 / degreesPerSegment + 1;
+		col = GetComponent<CircleCollider2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void StartRipple()
+	{
+		radialScale = 1f;
+		opacity = 1f;
+		line.SetVertexCount (vertexCount);
+		StartCoroutine ("Pulse");
+	}
+
+	public IEnumerator Pulse()
+	{
+		while (opacity > 0f)
+		{
+			Debug.Log ("Hi");
+			DrawVertexes();
+			col.radius = radialScale;
+			SetColors();
+			radialScale += growthRate;
+			opacity -= fadeRate;
+			yield return null;
+		}
+
+		line.SetVertexCount(0);
+		Touch.instance.rippleFinished (this);
+	}
+
+	void DrawVertexes()
+	{
+		float angle = 0;
+		for (int i = 0; i < vertexCount; ++i)
+		{
+			float rads = Mathf.Deg2Rad * angle;
+			
+			Vector3 pos = new Vector3(Mathf.Sin(rads), Mathf.Cos(rads),z);
+			line.SetPosition(i, pos*radialScale + transform.position);
+			angle += degreesPerSegment;
+		}
+	}
+	
+	void SetColors()
+	{
+		lineColor.a = opacity;
+		line.SetColors (lineColor, lineColor);
+	}
+}
---
+++ Assets/Scripts/Touch.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Touch : MonoBehaviour {
+
+	public int maxRipples;
+	public GameObject rippleObject;
+
+	public static Touch instance;
+	List<Ripple> ripples;
+	List<Ripple> inUse;
+	Camera cam;
+
+	// Use this for initialization
+	void Start () {
+		instance = this;
+		ripples = new List<Ripple> ();
+		inUse = new List<Ripple> ();
+
+		cam = Camera.main;
+
+		for(int i = 0; i < maxRipples; ++i)
+		{
+			GameObject ripOb = GameObject.Instantiate(rippleObject);
+
+			ripples.Add(ripOb.GetComponent<Ripple>());
+			Debug.Log (ripples[i]);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonUp(0))
+		{
+			if(ripples.Count > 0)
+			{
+				Ripple newRip = ripples[0];
+				Debug.Log (ripples[0]);
+				ripples.RemoveAt(0);
+				Debug.Log (newRip);
+				newRip.transform.position = (Vector2)cam.ScreenToWorldPoint(Input.mousePosition);
+				newRip.StartRipple();
+			}
+		}
+	}
+
+	public void rippleFinished(Ripple r)
+	{
+		ripples.Add (r);
+		inUse.Remove (r);
+	}
+}
